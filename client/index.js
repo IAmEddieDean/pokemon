b12fd1a3736fd7cbccca92e95de7672c87f9f9b6
Chris Oyler <christopher.oyler@gmail.com>
2015-04-14 11:16:04 -0700

    list made

--- client/index.js
+++ client/index.js
@@ -1,2 +1,19 @@
+/* global pokedex:true*/
+/* jshint camelcase:false*/
+
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init(){
+  drawPokedex();
+}
+function drawPokedex(){
+pokedex.pokemon.forEach(function(pokemonObj){
+  var $pokeDiv = $('<div>');
+  $pokeDiv.addClass('pokemon').attr('data-uri', pokemonObj.resource_uri);
+  var $pokeName = $('<div>');
+  $pokeName.addClass('name').text(pokemonObj.name);
+  $('#pokedex').append($pokeDiv.append($pokeName));
+});
+}
